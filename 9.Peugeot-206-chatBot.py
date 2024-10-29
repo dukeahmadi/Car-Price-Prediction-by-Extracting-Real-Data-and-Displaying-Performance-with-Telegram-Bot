30cdaa6e79afb0aae771d6f4b5eb5915c631c03b
amir ahmadi <162714606+dukeahmadi@users.noreply.github.com>
2024-10-29 20:28:10 +0330

    Update and rename Peugeot-206-chatBot.py to 9.Peugeot-206-chatBot.py

--- 9.Peugeot-206-chatBot.py
+++ 9.Peugeot-206-chatBot.py
@@ -21,8 +21,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     car[user_id] = []  # Initialize an empty list to store car info for the user
 
     # Ask the user to enter the car's manufacturing year
-    await update.message.reply_text("لطفا سال ساخت خودرو خود را به زبان انگلیسی وارد کنید:")
-
+    await update.message.reply_text("Please enter the year of your car in English:")
+    
 # Function to get the car's year from the user input
 async def get_year(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.effective_user.id
@@ -36,11 +36,11 @@ async def get_year(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if 1380 <= year <= current_year:
             car[user_id].append(year)  # Save the year if valid
             USER_STATE[user_id] = 'AWAITING_KILOMETERS'  # Move to the next step (kilometers input)
-            await update.message.reply_text("لطفا کارکرد خودرو خود را به زبان انگلیسی وارد کنید")
+            await update.message.reply_text("Please enter your vehicle operation in English")
         else:
-            await update.message.reply_text("لطفا یک عدد معتبر وارد کنید!")
+            await update.message.reply_text("Please enter a valid number!")
     except ValueError:
-        await update.message.reply_text("لطفا یک عدد معتبر وارد کنید!")
+        await update.message.reply_text("Please enter a valid number!")
 
 # Function to get the car's kilometers from the user input
 async def get_kilometers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -55,9 +55,9 @@ async def get_kilometers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
             USER_STATE[user_id] = 'AWAITING_MODEL'  # Move to the next step (model selection)
             await ask_trim(update, context)
         else:
-            await update.message.reply_text("لطفا یک عدد معتبر وارد کنید!")
+            await update.message.reply_text("Please enter a valid number!")
     except ValueError:
-        await update.message.reply_text("لطفا یک عدد معتبر وارد کنید!")
+        await update.message.reply_text("Please enter a valid number!")
 
 # Function to handle inline button presses and process the callback data
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -72,7 +72,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         USER_STATE[user_id] = 'AWAITING_PANORAMA'  # Move to the next step (Panorama query)
         await ask_panorama(update, context)
     elif USER_STATE.get(user_id) == 'AWAITING_PANORAMA':
-        car[user_id].append('بله' if query.data == '1' else 'خیر')  # Save panorama info
+        car[user_id].append('yes' if query.data == '1' else 'no')  # Save panorama info
         USER_STATE[user_id] = 'COMPLETED'  # Mark the process as completed
         await show_car_info(update, context)
 
@@ -90,22 +90,22 @@ async def ask_trim(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('لطفا تریم خودرو خود را انتخاب کنید:', reply_markup=reply_markup)
+    await update.message.reply_text('Please select your vehicle type:', reply_markup=reply_markup)
 
 # Function to ask if the car has a panorama roof via inline buttons
 async def ask_panorama(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("بله", callback_data='1'),
-            InlineKeyboardButton("خیر", callback_data='0'),
+            InlineKeyboardButton("yes", callback_data='1'),
+            InlineKeyboardButton("no", callback_data='0'),
         ],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
     if update.callback_query:
-        await update.callback_query.message.reply_text('آیا خودروی شما پانوراما است؟', reply_markup=reply_markup)
+        await update.callback_query.message.reply_text('Is your car panoramic?', reply_markup=reply_markup)
     else:
-        await update.message.reply_text('آیا خودروی شما پانوراما است؟', reply_markup=reply_markup)
+        await update.message.reply_text('Is your car panoramic?', reply_markup=reply_markup)
 
 
 # Function to show the car's collected information to the user and predict the price
@@ -119,11 +119,11 @@ async def show_car_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         year = car_info[0]
         kilometers = car_info[1]
         model = car_info[2]
-        panorama = 1 if car_info[3] == 'بله' else 0  # Convert 'بله/خیر' to 1/0
+        panorama = 1 if car_info[3] == 'yes' else 0  # Convert 'yes/no' to 1/0
         
         try:
             # Load the model from the .pkl file
-            with open("C:/Users/Alireza/Desktop/Car Price Prediction/Models/Peugeot206Model.pkl", 'rb') as file:
+            with open("C:/Users/Amir/Desktop/Car Price Prediction/Models/Peugeot206Model.pkl", 'rb') as file:
                 loaded_model = pickle.load(file)
             
             # Check if the loaded object is actually a model
@@ -136,29 +136,29 @@ async def show_car_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
             
                 # Prepare the response message
                 message = (
-                    f"اطلاعات خودروی شما:\n"
-                    f"سال ساخت: {year}\n"
-                    f"کارکرد: {kilometers} کیلومتر\n"
-                    f"تریم: تیپ {model}\n"
-                    f"پانوراما: {'بله' if panorama == 1 else 'خیر'}\n\n"
-                    f"قیمت تخمینی: {predicted_price:,.0f} تومان"
-                )
+                   f"Your vehicle information:\n"
+                   f"Year of manufacture: {year}\n"
+                   f"function: {kilometers} km\n"
+                   f"type: type {model}\n"
+                   f"Panorama: {'Yes' if panorama == 1 else 'No'}\n\n"
+                   f"Estimated price: {predicted_price:,.0f} Tomans"
+ )
             else:
-                message = "مدل به درستی لود نشده است. شیء بارگذاری شده متد 'predict' ندارد."
+                message = "The model was not loaded correctly. The loaded object does not have a 'predict' method."
         except FileNotFoundError:
-            message = "فایل مدل پیدا نشد. لطفاً مسیر فایل را بررسی کنید."
+            message = "Model file not found. Please check the file path."
         except Exception as e:
-            message = f"خطا در بارگذاری یا استفاده از مدل: {str(e)}"
+            message = f"Error loading or using model: {str(e)}"
     else:
-        message = "اطلاعات کامل خودرو در دسترس نیست."
+        message ="Full vehicle information is not available."
 
     # Send the result to the user
     if update.callback_query:
         await update.callback_query.message.reply_text(message)
-        await update.callback_query.message.reply_text("فرآیند تکمیل شد. برای شروع مجدد از دستور /start استفاده کنید.")
+        await update.callback_query.message.reply_text("Process completed. Use /start command to restart.")
     else:
         await update.message.reply_text(message)
-        await update.message.reply_text("فرآیند تکمیل شد. برای شروع مجدد از دستور /start استفاده کنید.")
+        await update.message.reply_text("Process completed. Use /start command to restart.")
 
 
 # Function to handle all incoming text messages
@@ -167,7 +167,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     state = USER_STATE.get(user_id)  # Check the user's current state
 
     if state == 'COMPLETED':
-        await update.message.reply_text("فرآیند قبلاً تکمیل شده است. برای شروع مجدد از دستور /start استفاده کنید.")
+        await update.message.reply_text("The process has already completed. Use the /start command to restart it.")
         return
 
     # Route the message based on the current user state
